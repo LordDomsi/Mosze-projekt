8f1d3a988a781b7acca761ee2aa9922f132062ec
kbence-sys44 <kadarbence2004@gmail.com>
2024-11-28 19:33:18 +0100

    shield fix

--- Assets/Scripts/GameBehaviour/Powerups.cs
+++ Assets/Scripts/GameBehaviour/Powerups.cs
@@ -7,16 +7,9 @@ using UnityEngine;
 public class Powerups : MonoBehaviour
 {
     [SerializeField] private List<GameObject> PowerUpList = new List<GameObject>();     // Power-upok lehetséges prefabjai, 0 a háromlövet, 1 a speed, 2 shield
-    private Rigidbody2D rigidBody;
     public int PowerUpID = 0;      //melyik power-up
 
     private const string PLAYER_TAG = "Player";
-
-
-    private void Awake()
-    {
-        rigidBody = GetComponent<Rigidbody2D>();
-    }
 
     public void PowerUpSpawn(Vector2 spawnPos)
     {
@@ -27,16 +20,11 @@ public class Powerups : MonoBehaviour
 
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == PLAYER_TAG)
         {
             Destroy(this.gameObject);
         }
-    }
-
-        private void Update()
-    {
-        
     }
 }
