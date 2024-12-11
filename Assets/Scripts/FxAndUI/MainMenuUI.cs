6fbf142f9efc5255631b9c241faddeb68ad7b07b
kbence-sys44 <kadarbence2004@gmail.com>
2024-12-11 18:32:14 +0100

    namespace fix for build

--- Assets/Scripts/FxAndUI/MainMenuUI.cs
+++ Assets/Scripts/FxAndUI/MainMenuUI.cs
@@ -2,8 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using Unity.VisualScripting;
-using UnityEditor.Animations;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
